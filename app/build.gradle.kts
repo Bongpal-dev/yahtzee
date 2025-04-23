9b54863f6b8a0714291a815f91ba477137cc2b5f
bong-pal <etgt777@naver.com>
2025-04-23 22:19:58 +0900

    Chore: 버전 증가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     defaultConfig {
         applicationId = "com.bongpal.yatzee.app"
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 4
+        versionName = "0.2.0"
     }
 
     signingConfigs {
